15d00a44eae83d67df4a549705aedd09f8b48180
sotoi <sotoi@uci.edu>
2017-02-04 20:04:11 -0800

    checkout finished test with someone from the credit card database

--- Isaacproj2/src/Login.java
+++ Isaacproj2/src/Login.java
@@ -4,7 +4,7 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.sql.*;
-
+import HelperClasses.*;
 
 public class Login extends HttpServlet {
 //	private static final long serialVersionUID = 1L;
@@ -23,9 +23,11 @@ public class Login extends HttpServlet {
 				
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
-		
-		if(checkUser(email, password)) {
+		Customer check= checkUser(email, password);
+		if(check!=null) {
 //			response.sendRedirect(request.getContextPath() + "/mainpage.html");
+			mySession.setAttribute("cart_session", new cartSession());
+			mySession.setAttribute("valid_customer",check );
 			response.sendRedirect("mainpage.html");
 
 		}
@@ -35,25 +37,28 @@ public class Login extends HttpServlet {
 		}
 	}
 	
-	public boolean checkUser(String email, String password) {
+	public static Customer checkUser(String email, String password) {
 
-		boolean answer = false;
+		Customer valid_customer= null;
 		try {
 //			System.out.printf("%s %s", email, password);
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/moviedb?useSSL=false", "isinger", "pi3zza");
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/moviedb?useSSL=false", "root", "root");
 			Statement select = connection.createStatement();
 			String query = "SELECT * FROM customers WHERE email='" + email + "' AND password='" + password + "'";
 			ResultSet check = select.executeQuery(query);
 			
-			answer = check.next();
+			if(check.next()){
+				valid_customer = new Customer(check.getInt(1),check.getString(2),check.getString(3),check.getString(4), check.getString(5),check.getString(6),check.getString(7));
+				
+			}
+			return valid_customer;
 //			System.out.println(answer);
-			
 			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		return answer;
+		return valid_customer;
 	}
 
 }
