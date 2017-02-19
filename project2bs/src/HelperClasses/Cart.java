c258c08c607a5ba511a0e47d67e3fa1f910960b7
Raymond Hsu <hsurw@uci.edu>
2017-02-19 14:34:18 -0800

    fixing git repo pt2

---
+++ project2bs/src/HelperClasses/Cart.java
@@ -0,0 +1,32 @@
+package HelperClasses;
+
+public class Cart {
+	private Movie movie;
+	private int quantity;
+	public Cart(){}
+	public Cart(Movie movie,int quantity){
+		super();
+		this.movie=movie;
+		this.quantity=quantity;
+		
+	}
+	public Movie getMovie() {
+		return movie;
+	}
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public void addQuantity(int quantity){
+		this.quantity+=quantity;
+	}
+	public void removeQuantity(int quantity ){
+		this.quantity-=quantity;
+	}
+	
+}
