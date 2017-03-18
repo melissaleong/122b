0640627fb4361e9fc8b1247d215cf2c834bd2099
Raymond Hsu <hsurw@uci.edu>
2017-03-17 17:17:01 -0700

    modified proj4_ajax for connection pool/no prep statement jmeter testing

--- proj4_ajax/src/HelperClasses/Database.java
+++ proj4_ajax/src/HelperClasses/Database.java
@@ -4,6 +4,9 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.sql.*;
+import javax.naming.InitialContext;
+import javax.naming.Context;
+import javax.sql.DataSource;
 
 public class Database {
 //	private String url = "jdbc:mysql://localhost:3306/moviedb?useSSL=false";
@@ -13,8 +16,13 @@ public class Database {
 	public static Connection openConnection() throws ClassNotFoundException{
 		Connection connection = null;
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/moviedb?useSSL=false", "root", "root1234");
+			// Class.forName("com.mysql.jdbc.Driver");
+			// connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/moviedb?useSSL=false", "root", "root1234");
+			Context initCtx = new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+			DataSource ds = (DataSource) envCtx.lookup("jdbc/moviedb");
+			
+			Connection connection = ds.getConnection();
 			return connection;
 
 		} catch (Exception e) {
